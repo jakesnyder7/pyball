30d7d5f3bdc175db4e6d6cf398efe3348462a398
Marion Geary <77766742+mfgeary@users.noreply.github.com>
2022-02-23 23:18:52 -0600

    Functional homepage!
    The homepage works! I hope to do some tidying and creating more of a
    navbar/menu soon, but this definitely works for our purposes now.

--- pyball/src/Home/Home.js
+++ pyball/src/Home/Home.js
@@ -5,10 +5,13 @@ import './Home.css';
 function Home() {
     return (
         <div>
+          <nav>
           <div>
+            <NavLink to="/">
             <button class="button-16" role="button">
               Reset
             </button>
+            </NavLink>
           </div>
           <div className="App">
             <header>
@@ -17,17 +20,24 @@ function Home() {
               </p>
             </header>
             <div className="Prototype">
-              <button class="button-3" role="button" >
-                Compare Two Players
-              </button>
-              <button class="button-3" role="button">
-                Manipulate Spreadsheet
-              </button>
-              <button class="button-3" role="button">
-                Edit Team
-              </button>
+              <NavLink to="/compare-players">
+                <button class="button-3" role="button" >
+                  Compare Two Players
+                </button>
+              </NavLink>
+              <NavLink class="button-3" role="button" to="/manipulate-spreadsheet">
+                <button class="button-3" role="button">
+                  Manipulate Spreadsheet
+                </button>
+              </NavLink>
+              <NavLink to="/edit-team">
+                <button class="button-3" role="button">
+                  Edit Team
+                </button>
+              </NavLink>
             </div>
           </div>
+          </nav>
         </div>
       );
 }
