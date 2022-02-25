a883151a6e6fb799480300d1d18a4125a38a7ee9
cw353 <claire@claires-air.student.wheaton.edu>
2022-02-25 10:18:59 -0600

    first draft of ManipulatableSpreadsheet (supports single- and multi-column sorting and basic conditional formatting)

---
+++ pyball/src/ManipulateSpreadsheet/ManipulatableTable.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { useTable, useSortBy, useRowState } from 'react-table';
+
+/**
+ * Hook to define a table that supports various manipulations.
+ * Currently supports limited conditional formatting and sorting by single or multiple columns.
+ * Uses react-table.
+ * @author Claire Wagner
+ * @param columns The columns to display in the table
+ * (must conform to react-table specifications: https://react-table.tanstack.com/docs/api/useTable).
+ * @param data The data to display in the table
+ * (must conform to react-table specifications: https://react-table.tanstack.com/docs/api/useTable).
+ * @returns A div containing the table.
+ * Portions of this hook are based on https://stackoverflow.com/a/69128343 (CC BY-SA 4.0 license),
+ * https://github.com/TanStack/react-table/blob/v7/examples/basic/src/App.js (MIT license), and
+ * https://github.com/TanStack/react-table/tree/v7/examples/sorting (MIT license).
+ */
+export function ManipulatableTable({columns, data}) {
+
+    // The table instance
+    const table = useTable(
+        {
+            columns: columns,
+            data: data,
+            disableSortRemove: true,
+            initialCellStateAccessor: cell => ({ backgroundColor: null })
+        },
+        useSortBy,
+        useRowState
+    );
+
+    /**
+     * Function to apply conditional formatting to the cells in the specified column.
+     * Applies the specified background color to all cells in that column with values
+     * that are less than the comparand.
+     * @param comparand The comparand against which to compare cell values.
+     * @param columnID The id of the column to which to apply the conditional formatting.
+     * @param color The background color to apply.
+     * Postcondition: The background colors of all cells in the given column with values that are
+     * less than the comparand have been set to the specified color. The background colors of all
+     * other cells in that column have been set to null. No other cell's background color has been
+     * changed.
+    */
+    function applyConditionalFormatting(comparand, columnID, color) {
+        //const columnID = 'td';
+        //const comparand = e.target.value;
+        // apply conditional formatting if that 
+        if (!isNaN(parseInt(comparand))) {
+            table.rows.forEach(row => {
+                row.cells.forEach(cell => {
+                    cell.setState( (oldval) => {
+                        return (
+                            { backgroundColor: 
+                                cell.column.id === columnID ? (cell.value < comparand ? color : null) : oldval}
+                        );
+                    });
+                })
+            });
+        }
+    };
+
+    // Render the table UI 
+    return (
+        <div>
+            <table {...table.getTableProps()}>
+                <thead>
+                    {table.headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                    {column.render('Header')}
+                                    <span>
+                                        {column.isSorted ? (column.isSortedDesc ? ' 🔽': ' 🔼') : ''}
+                                    </span>
+                                    {column.id !== 'name' &&
+                                    <div>
+                                        <label>
+                                            {'highlight stats less than: '}<br></br>
+                                            <input placeholder='enter comparand' onChange={ (e) => 
+                                                { applyConditionalFormatting(
+                                                    e.target.value, column.id, 'cyan');
+                                                } 
+                                            } />
+                                        </label>
+                                    </div>}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...table.getTableBodyProps()}>
+                    {table.rows.map((row, i) => {
+                        table.prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td {...cell.getCellProps([{
+                                            style: { backgroundColor: cell.state.backgroundColor }
+                                        }])}>
+                                            {cell.render('Cell')}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
--- pyball/src/ManipulateSpreadsheet/ManipulateSpreadsheet.js
+++ pyball/src/ManipulateSpreadsheet/ManipulateSpreadsheet.js
@@ -1,17 +1,60 @@
 import Navigation from '../Navigation/Navigation';
+import React from 'react';
+import { ManipulatableTable } from './ManipulatableTable.js';
 
+/**
+ * Hook to define and display the manipulatable spreadsheet page.
+ * @author Claire Wagner
+ * @returns A div containing all page elements.
+ */
 function ManipulateSpreadsheet() {
-    return (
+
+  // hard-coded data for prototype (will be provided by backend in final product)
+  const columns = React.useMemo(
+    () => [
+        {Header: 'Name', accessor: 'name'},
+        {Header: 'TD', accessor: 'td'},
+        {Header: 'INT', accessor: 'int'},
+    ],
+    []
+  )
+
+  // hard-coded data for prototype (will be provided by backend in final product)
+  const data = React.useMemo(
+    () => [
+        {name: 'Tom Brady', td: 43, int: 12},
+        {name: 'Justin Herbert', td: 38, int: 15},
+        {name: 'Matthew Stafford', td: 41, int: 17},
+        {name: 'Patrick Mahomes', td: 37, int: 13},
+        {name: 'Josh Allen', td: 36, int: 15},
+        {name: 'Kirk Cousins', td: 33, int: 7},
+        {name: 'Aaron Rodgers', td: 37, int: 4},
+        {name: 'Jimmy Garoppolo', td: 20, int: 12},
+        {name: 'Mac Jones', td: 22, int: 13},
+        {name: 'Ben Roethlisberger', td: 22, int: 10},
+        {name: 'Carson Wentz', td: 27, int: 7},
+        {name: 'Jared Goff', td: 19, int: 8},
+        {name: 'Jalen Hurts', td: 16, int: 9},
+        {name: 'Russell Wilson', td: 25, int: 6},
+        {name: 'Teddy Bridgewater', td: 18, int: 7},
+        {name: 'Lamar Jackson', td: 16, int: 13}
+    ],
+    []
+  );
+
+  return (
+    <div>
+      <Navigation />
       <div>
-        <Navigation />
-        <div>
-          <header>
-            <p>
-                Here is the Manipulate Spreadsheet page!
-            </p>
-          </header>
-        </div>
+        <header>
+          <p>
+            Click a column header to sort by that column 
+            (hold down the shift key to sort by multiple columns at a time). 
+          </p>
+        </header>
       </div>
+      <ManipulatableTable columns={columns} data={data} />
+    </div>
     );
   }
   
