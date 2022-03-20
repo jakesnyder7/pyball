7f100cbf76d775bf7dd5f3392bc7b195910d8753
Marion Geary <77766742+mfgeary@users.noreply.github.com>
2022-03-20 15:33:31 -0500

    Fix documentation for R functions

--- prototype/backend/ParseNFLPlayers.R
+++ prototype/backend/ParseNFLPlayers.R
@@ -1,14 +1,21 @@
+# load in necessary libraries
 library(nflverse)
 library(tidymodels)
 library(tidyverse)
 
+# turn off warnings on console
 options(warn=-1)
 
+# load in needed data frames from nflverse
 player_stats <- load_player_stats(seasons = most_recent_season())
 player_stats_kicker <- load_player_stats(seasons = most_recent_season(), stat_type = "kicking")
 roster <- load_rosters(seasons = most_recent_season())
 team_data <- load_teams()
 
+#' Get the data about a specified player
+#' 
+#' @param player_name_query The name of the desired player
+#' @return Data on the player formatted as a JSON
 get_player_data <- function(player_name_query) {
   filtered_player_roster <- roster %>% filter(full_name == player_name_query) %>% select(-season)
   if(filtered_player_roster$position == 'K') {
@@ -37,12 +44,16 @@ get_player_data <- function(player_name_query) {
   return(player_json)
 }
 
+# examples highlighting the two cases for player data return
 get_player_data("Tom Brady")
 get_player_data("Trace McSorley")
 
+#' Get data on all players in a specified position
+#' 
+#' @param position_query The official abbreviation of the position
+#' @return Data on all players in the specified position formatted as a JSON
 get_position_players <- function(position_query) {
-  position_roster <- roster %>% filter(position == position_query)
-  ran = FALSE
+  position_roster <- roster %>% filter(position == position_query | depth_chart_position == position_query)
   position_info <- list()
   for(player in position_roster$full_name) {
     player_data <- jsonlite::fromJSON(get_player_data(player))
@@ -51,13 +62,5 @@ get_position_players <- function(position_query) {
   position_info_json <- jsonlite::toJSON(position_info, pretty=TRUE)
   return(position_info_json)
 }
-
-#    if(!ran) {
-#  position_info <- list(player = player_data)
-# position_info[[player]] <- player_data
-# } else {
-#   position_info[[player]] <- player_data
-# } 
-# ran = TRUE
 
 get_position_players("QB")
