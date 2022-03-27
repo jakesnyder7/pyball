ddc969b5374117b458e41b2f72886e87090240f0
cw353 <claire@claires-air.student.wheaton.edu>
2022-03-27 18:45:53 -0500

    Small changes to ManipulateSpreadsheet to improve efficiency and clarity; temporarily commented out code related to TE tab to improve loading times for testing

--- prototype/pyball/src/ManipulateSpreadsheet/ManipulateSpreadsheet.js
+++ prototype/pyball/src/ManipulateSpreadsheet/ManipulateSpreadsheet.js
@@ -6,20 +6,6 @@ import './ManipulateSpreadsheet.css';
 import Got from '../api/Got.js';
 
 /**
- * Helper function to check if all values in an array are truthy.
- * @param array The array to check.
- * @returns True if all values are truthy, false otherwise.
- */
-function allTruthy(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (!array[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-/**
  * Hook to define and display the manipulatable spreadsheet page.
  * @author Claire Wagner
  * @returns A div containing all page elements.
@@ -49,6 +35,12 @@ function ManipulateSpreadsheet() {
         {Header: 'Rec Avg', accessor: 'receptions', filter: 'startswith', formattable: true},
         {Header: 'Rec Yd Avg', accessor: 'receiving_yards', filter: 'startswith', formattable: true},
         {Header: 'Rec TD Avg', accessor: 'receiving_tds', filter: 'startswith', formattable: true}
+      ],
+      TE: [
+        {Header: 'Player', accessor: 'full_name', filter: 'any_word_startswith', formattable: false},
+        {Header: 'Rec Avg', accessor: 'receptions', filter: 'startswith', formattable: true},
+        {Header: 'Rec Yd Avg', accessor: 'receiving_yards', filter: 'startswith', formattable: true},
+        {Header: 'Rec TD Avg', accessor: 'receiving_tds', filter: 'startswith', formattable: true}
       ]
     }),
     []
@@ -57,14 +49,23 @@ function ManipulateSpreadsheet() {
   // State to keep track of the data for each table
   const [data1, setData1] = React.useState('');
   const [data2, setData2] = React.useState('');
+  const [data3, setData3] = React.useState('');
+  //const [data4, setData4] = React.useState('');
 
   // The positions and associated data for the tables
   const tables = React.useMemo (
     () => ([
       { position: 'QB', data: data1, setData: setData1 },
       { position: 'RB', data: data2, setData: setData2 },
+      { position: 'WR', data: data3, setData: setData3 },
+      //{ position: 'TE', data: data4, setData: setData4 },
     ]),
-    [data1, data2]
+    [
+      data1,
+      data2,
+      data3,
+      //data4,
+    ]
   );
 
   // Fetch player data (should only occur once)
@@ -92,12 +93,22 @@ function ManipulateSpreadsheet() {
     [tables, columns]
   );
 
+  // Helper function to check if all data has been fetched.
+  function allDataFetched() {
+    for (let i = 0; i < tables.length; i++) {
+      if (!tables[i].data) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   return (
     <div>
       <Navigation />
       <div>
         { /* Render tabs only if all data has been fetched */ }
-        {allTruthy(tables.map((table) => table.data))
+        {allDataFetched()
           ? <Tabs tabs={tabs}/>
           : <header>{"Fetching data..."}</header>}
       </div>
