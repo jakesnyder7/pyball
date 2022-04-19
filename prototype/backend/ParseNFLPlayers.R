4487d2b031193da88f74b6cd7815b52200f86abc
Marion Geary <77766742+mfgeary@users.noreply.github.com>
2022-04-19 12:38:53 -0500

    Remove warnings on server start up

--- prototype/backend/ParseNFLPlayers.R
+++ prototype/backend/ParseNFLPlayers.R
@@ -6,9 +6,13 @@
 #' @author Marion Geary
 #' March 22, 2022
 
-library(nflverse)
-library(tidyverse)
-library(stringr)
+defaultW <- getOption("warn")
+options(warn = -1)
+
+options(nflreadr.verbose = FALSE)
+(library(nflverse))
+(library(tidyverse))
+
 
 #setwd("/Users/Marion/Desktop/csci335/pyball/prototype/backend")
 
@@ -16,182 +20,182 @@ load('APIData.Rdata')
 
 # load in needed data frames from nflverse
 
-ff_rankings <- load_ff_rankings()
-
-ff_rankings <- ff_rankings %>%
-  filter(str_starts(page_type, "redraft")) %>%
-  filter(page_type != "redraft-op") %>%
-  filter(page_type != "redraft-overall") %>%
-  filter(page_type != "redraft-lb") %>%
-  filter(page_type != "redraft-idp") %>%
-  select(yahoo_id, ecr, page_type) %>%
-  mutate(yahoo_id = as.numeric(yahoo_id))
-
-#ff_rankings %>% mutate(player = as.factor(player)) %>% group_by(player) %>% summarize(player, count = n()) %>% arrange(-count)
-
-
-official_player_stats <- calculate_player_stats(nflreadr::load_pbp(), weekly = TRUE) %>% filter(season_type == 'REG')
-
-official_player_stats_total <- calculate_player_stats(nflreadr::load_pbp(), weekly = FALSE)
-
-player_stats_kicking <- nflreadr::load_player_stats(seasons = most_recent_season(), stat_type = 'kicking')
-
-combine <- load_combine(seasons = TRUE)
-
-roster <- load_rosters(seasons = most_recent_season())
-
-team_data <- load_teams()
-
-injuries <- load_injuries()
-
-next_gen_pass <- load_nextgen_stats(
-  seasons = most_recent_season(),
-  stat_type = 'passing'
-)
-
-next_gen_rec <- load_nextgen_stats(
-  seasons = most_recent_season(),
-  stat_type = 'receiving'
-)
-
-next_gen_rush <- load_nextgen_stats(
-  seasons = most_recent_season(),
-  stat_type = 'rushing'
-)
-
-snap_counts <- load_snap_counts(seasons = most_recent_season())
-
-# Join the data :)
-
-roster_off <- roster %>%
-  full_join(
-    official_player_stats,
-    by = c("gsis_id" = "player_id"),
-    suffix = c('', '.xyz'),
-    keep = FALSE,
-    na_matches = 'never'
-  ) 
-
-roster_off <- roster_off %>%
-  distinct() %>%
-  select(- (grep(".xyz", names(roster_off))))
-
-roster_team_off <- roster_off %>%
-  left_join(
-    team_data,
-    by = c("recent_team" = "team_abbr"),
-    suffix = c('', '.xyz'),
-    keep = FALSE,
-    na_matches = 'never'
-  ) 
-
-roster_team_off <- roster_team_off %>%
-  distinct() %>%
-  select(- (grep(".xyz", names(roster_team_off))))
-
-roster_off_kick <- roster_team_off %>%
-  full_join(
-    player_stats_kicking,
-    by = c("gsis_id" = "player_id"),
-    suffix = c('', '.xyz'),
-    keep = FALSE,
-    na_matches = 'never'
-  ) 
-
-roster_off_kick <- roster_off_kick %>%
-  distinct() %>%
-  select(- (grep(".xyz", names(roster_off_kick))))
-
-roster_off_pl_kick_com <- roster_off_kick %>%
-  left_join(
-    combine,
-    by = c('pfr_id' = 'pfr_id'),
-    suffix = c('', '.xyz'),
-    keep = FALSE,
-    na_matches = 'never'
-  )
-
-roster_off_pl_kick_com <- roster_off_pl_kick_com %>%
-  distinct() %>%
-  select(- (grep(".xyz", names(roster_off_pl_kick_com)))) 
-
-injuries <- injuries %>%
-  mutate(season = as.numeric(season), week = as.numeric(week))
-
-roster_off_pl_kick_com_team_inj <- roster_off_pl_kick_com %>%
-  left_join(
-    injuries,
-    by = c('gsis_id' = 'gsis_id', 'season' = 'season', 'week' = 'week'),
-    suffix = c('', '.xyz'),
-    keep = FALSE
-  )
-
-roster_off_pl_kick_com_team_inj <- roster_off_pl_kick_com_team_inj %>%
-  distinct() %>%
-  select(- (grep(".xyz", names(roster_off_pl_kick_com_team_inj)))) 
-
-all_next_gen <- roster_off_pl_kick_com_team_inj %>%
-  left_join(
-    next_gen_pass,
-    by = c('gsis_id' = 'player_gsis_id', 'season' = 'season', 'week' = 'week'),
-    suffix = c('', '.xyz'),
-    keep = FALSE
-  ) %>% left_join(
-    next_gen_rec,
-    by = c('gsis_id' = 'player_gsis_id', 'season' = 'season', 'week' = 'week'),
-    suffix = c('', '.xyz'),
-    keep = FALSE
-  ) %>% left_join(
-    next_gen_rush,
-    by = c('gsis_id' = 'player_gsis_id', 'season' = 'season', 'week' = 'week'),
-    suffix = c('', '.xyz'),
-    keep = FALSE
-  )
-
-all_next_gen <- all_next_gen %>%
-  distinct() %>%
-  select(- (grep(".xyz", names(all_next_gen))))
-
-all_data <- all_next_gen %>% left_join(
-  snap_counts,
-  by = c('pfr_id' = 'pfr_player_id', 'season' = 'season', 'week' = 'week'),
-  suffix = c('', '.xyz'),
-  keep = FALSE
-)
-
-all_data <- all_data %>%
-  distinct() %>%
-  select(- (grep(".xyz", names(all_data))))
-
-all_data <- all_data %>%
-  select(-c(espn_id, sportradar_id, rotowire_id, pff_id,
-            fantasy_data_id, sleeper_id, player_name, cfb_id, pos, date_modified,
-            player_display_name, team_abbr, player_position, player_first_name,
-            player_last_name, player_short_name, player_jersey_number,
-            pfr_game_id, season_type, player, high_school,
-            season, last_name, first_name))
-
-all_data <- all_data %>%
-  left_join(
-    ff_rankings,
-    by = c('yahoo_id' = 'yahoo_id'),
-    suffix = c('', '.xyz'),
-    keep = FALSE,
-    na_matches = "never"
-  ) %>% 
-  left_join(
-    official_player_stats_total,
-    by = c('gsis_id' = 'player_id'),
-    suffix = c('', '_total'),
-    keep = FALSE,
-    na_matches = "never"
-  ) %>% distinct()
-
-all_data <- all_data %>% mutate_if(is.character, as.factor) %>% filter(!is.na(full_name))
-
-all_data <- all_data %>%
-  distinct() %>%
-  select(- (grep(".xyz", names(all_data))))
+# ff_rankings <- load_ff_rankings()
+# 
+# ff_rankings <- ff_rankings %>%
+#   filter(str_starts(page_type, "redraft")) %>%
+#   filter(page_type != "redraft-op") %>%
+#   filter(page_type != "redraft-overall") %>%
+#   filter(page_type != "redraft-lb") %>%
+#   filter(page_type != "redraft-idp") %>%
+#   select(yahoo_id, ecr, page_type) %>%
+#   mutate(yahoo_id = as.numeric(yahoo_id))
+# 
+# #ff_rankings %>% mutate(player = as.factor(player)) %>% group_by(player) %>% summarize(player, count = n()) %>% arrange(-count)
+# 
+# 
+# official_player_stats <- calculate_player_stats(nflreadr::load_pbp(), weekly = TRUE) %>% filter(season_type == 'REG')
+# 
+# official_player_stats_total <- calculate_player_stats(nflreadr::load_pbp(), weekly = FALSE)
+# 
+# player_stats_kicking <- nflreadr::load_player_stats(seasons = most_recent_season(), stat_type = 'kicking')
+# 
+# combine <- load_combine(seasons = TRUE)
+# 
+# roster <- load_rosters(seasons = most_recent_season())
+# 
+# team_data <- load_teams()
+# 
+# injuries <- load_injuries()
+# 
+# next_gen_pass <- load_nextgen_stats(
+#   seasons = most_recent_season(),
+#   stat_type = 'passing'
+# )
+# 
+# next_gen_rec <- load_nextgen_stats(
+#   seasons = most_recent_season(),
+#   stat_type = 'receiving'
+# )
+# 
+# next_gen_rush <- load_nextgen_stats(
+#   seasons = most_recent_season(),
+#   stat_type = 'rushing'
+# )
+# 
+# snap_counts <- load_snap_counts(seasons = most_recent_season())
+# 
+# # Join the data :)
+# 
+# roster_off <- roster %>%
+#   full_join(
+#     official_player_stats,
+#     by = c("gsis_id" = "player_id"),
+#     suffix = c('', '.xyz'),
+#     keep = FALSE,
+#     na_matches = 'never'
+#   ) 
+# 
+# roster_off <- roster_off %>%
+#   distinct() %>%
+#   select(- (grep(".xyz", names(roster_off))))
+# 
+# roster_team_off <- roster_off %>%
+#   left_join(
+#     team_data,
+#     by = c("recent_team" = "team_abbr"),
+#     suffix = c('', '.xyz'),
+#     keep = FALSE,
+#     na_matches = 'never'
+#   ) 
+# 
+# roster_team_off <- roster_team_off %>%
+#   distinct() %>%
+#   select(- (grep(".xyz", names(roster_team_off))))
+# 
+# roster_off_kick <- roster_team_off %>%
+#   full_join(
+#     player_stats_kicking,
+#     by = c("gsis_id" = "player_id"),
+#     suffix = c('', '.xyz'),
+#     keep = FALSE,
+#     na_matches = 'never'
+#   ) 
+# 
+# roster_off_kick <- roster_off_kick %>%
+#   distinct() %>%
+#   select(- (grep(".xyz", names(roster_off_kick))))
+# 
+# roster_off_pl_kick_com <- roster_off_kick %>%
+#   left_join(
+#     combine,
+#     by = c('pfr_id' = 'pfr_id'),
+#     suffix = c('', '.xyz'),
+#     keep = FALSE,
+#     na_matches = 'never'
+#   )
+# 
+# roster_off_pl_kick_com <- roster_off_pl_kick_com %>%
+#   distinct() %>%
+#   select(- (grep(".xyz", names(roster_off_pl_kick_com)))) 
+# 
+# injuries <- injuries %>%
+#   mutate(season = as.numeric(season), week = as.numeric(week))
+# 
+# roster_off_pl_kick_com_team_inj <- roster_off_pl_kick_com %>%
+#   left_join(
+#     injuries,
+#     by = c('gsis_id' = 'gsis_id', 'season' = 'season', 'week' = 'week'),
+#     suffix = c('', '.xyz'),
+#     keep = FALSE
+#   )
+# 
+# roster_off_pl_kick_com_team_inj <- roster_off_pl_kick_com_team_inj %>%
+#   distinct() %>%
+#   select(- (grep(".xyz", names(roster_off_pl_kick_com_team_inj)))) 
+# 
+# all_next_gen <- roster_off_pl_kick_com_team_inj %>%
+#   left_join(
+#     next_gen_pass,
+#     by = c('gsis_id' = 'player_gsis_id', 'season' = 'season', 'week' = 'week'),
+#     suffix = c('', '.xyz'),
+#     keep = FALSE
+#   ) %>% left_join(
+#     next_gen_rec,
+#     by = c('gsis_id' = 'player_gsis_id', 'season' = 'season', 'week' = 'week'),
+#     suffix = c('', '.xyz'),
+#     keep = FALSE
+#   ) %>% left_join(
+#     next_gen_rush,
+#     by = c('gsis_id' = 'player_gsis_id', 'season' = 'season', 'week' = 'week'),
+#     suffix = c('', '.xyz'),
+#     keep = FALSE
+#   )
+# 
+# all_next_gen <- all_next_gen %>%
+#   distinct() %>%
+#   select(- (grep(".xyz", names(all_next_gen))))
+# 
+# all_data <- all_next_gen %>% left_join(
+#   snap_counts,
+#   by = c('pfr_id' = 'pfr_player_id', 'season' = 'season', 'week' = 'week'),
+#   suffix = c('', '.xyz'),
+#   keep = FALSE
+# )
+# 
+# all_data <- all_data %>%
+#   distinct() %>%
+#   select(- (grep(".xyz", names(all_data))))
+# 
+# all_data <- all_data %>%
+#   select(-c(espn_id, sportradar_id, rotowire_id, pff_id,
+#             fantasy_data_id, sleeper_id, player_name, cfb_id, pos, date_modified,
+#             player_display_name, team_abbr, player_position, player_first_name,
+#             player_last_name, player_short_name, player_jersey_number,
+#             pfr_game_id, season_type, player, high_school,
+#             season, last_name, first_name))
+# 
+# all_data <- all_data %>%
+#   left_join(
+#     ff_rankings,
+#     by = c('yahoo_id' = 'yahoo_id'),
+#     suffix = c('', '.xyz'),
+#     keep = FALSE,
+#     na_matches = "never"
+#   ) %>% 
+#   left_join(
+#     official_player_stats_total,
+#     by = c('gsis_id' = 'player_id'),
+#     suffix = c('', '_total'),
+#     keep = FALSE,
+#     na_matches = "never"
+#   ) %>% distinct()
+# 
+# all_data <- all_data %>% mutate_if(is.character, as.factor) %>% filter(!is.na(full_name))
+# 
+# all_data <- all_data %>%
+#   distinct() %>%
+#   select(- (grep(".xyz", names(all_data))))
 
 # get_player_data("Tom Brady")
 
