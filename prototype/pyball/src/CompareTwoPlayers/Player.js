7294d41d0672b95adf90486d347fb1530909d416
Pedro Avalos <pavalos6401@gmail.com>
2022-04-21 11:08:35 -0500

    center and circle player in compare players

--- prototype/pyball/src/CompareTwoPlayers/Player.js
+++ prototype/pyball/src/CompareTwoPlayers/Player.js
@@ -11,8 +11,10 @@ import './Player.css';
 export function Player({ player }) {
     return (
         <div className='Player' >
-            <img class="img-player" src={player.headshot_url} alt={player.full_name} height={200} />
-             <div>
+            <div class="img-crop">
+                <img class="img-player" src={player.headshot_url} alt={player.full_name} height={200} />
+            </div>
+            <div>
                 <img class="img-logo" src={player.team_logo_espn} alt={player.team_nick} height={50} />
                 <p>{player.team_name}</p>
             </div>
